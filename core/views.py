33dbcccf571d7cb89fbead7517532ff3817fdbf5
Lindokuhle Sibeko <nicsibeko03@gmail.com>
2025-08-11 23:54:05 +0200

    Just need to commit

--- core/views.py
+++ core/views.py
@@ -3,5 +3,19 @@ from django.shortcuts import render
 def home(request):
     return render(request, "core/home.html", {})
 
-#def dashboard(request):
-    #return render(request, "core/dashboard.html")
+def dashboard(request):
+    return render(request, "core/dashboard.html")
+
+def add_expense(request):
+    if request.method == "POST":
+        expense = request.POST.get("expense")
+        price = request.POST.get("price")
+        date = request.POST.get("date")
+        
+        # Here you would typically save the expense to the database
+        # For example:
+        # Expense.objects.create(expense=expense, price=price, date=date)
+        
+        return render(request, "core/dashboard.html", {"message": "Expense added successfully!"})
+    
+    return render(request, "core/add_expense.html")
--- users/views.py
+++ users/views.py
@@ -17,3 +17,17 @@ def SignUp(request):
         form = RegisterForm()
 
     return render(request, 'users/signup.html', {'form':form})    
+
+def LogIn(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            #messages.success(request, ('You have been logged in'))
+            return redirect('dashboard')
+        else:
+            #messages.error(request, ('There was an error logging in, please try again'))
+            return render(request, 'users/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'users/login.html')
